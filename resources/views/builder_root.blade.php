758bab0cb7cffd99b5a3baaa160c918c05f6e229
linhnq <linhnq23062000@gmail.com>
2025-04-23 14:50:02 +0700

    add change read file css js real not with cache

--- resources/views/builder_root.blade.php
+++ resources/views/builder_root.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>{{config('app.name')}} | Page Builder</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{asset('css/laravel-grapes.css')}}" rel="stylesheet">
+    <link href="{{asset('css/laravel-grapes.css?v=') . time() }}" rel="stylesheet">
     <style>
         body,
         html {
@@ -34,6 +34,6 @@
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="{{ asset('/js/laravel-grapes.js') }}"></script>
+    <script src="{{ asset('/js/laravel-grapes.js?v=') . time() }}"></script>
 </body>
 </html>
